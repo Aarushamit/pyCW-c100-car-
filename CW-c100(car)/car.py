fe1aa429d649a838f8b5510c626c1b629651e96a
Aarushamit <64181805+Aarushamit@users.noreply.github.com>
2021-02-03 19:01:38 +0530

    Add files via upload

---
+++ CW-c100(car)/car.py
@@ -0,0 +1,11 @@
+class Car(object):
+    def __init__(self, brand, colour, fuelType, mileage=None ):
+        self.brand = brand
+        self.colour = colour
+        self.fuelType = fuelType
+        self.mileage = mileage
+
+    def setModel(self, model, brand):
+        self.brand[model] = brand
+    def Getmodel(self, model):
+       returnk self.brand[model] 
